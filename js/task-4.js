02e2de3fd267986990148adbc84bd5a8591c90c0
dmytro-b-fs <dmytro.burakov@gmail.com>
2026-05-18 18:44:25 +0300

    hw7upd

--- js/task-4.js
+++ js/task-4.js
@@ -3,13 +3,13 @@ const ourForm = document.querySelector(".login-form")
 
 const ourFormCallback = (event) => {
     event.preventDefault();
-    if (event.target.elements.email.value==="" || event.target.elements.password.value===""){
+    if (event.target.elements.email.value.trim()==="" || event.target.elements.password.value.trim()===""){
         window.alert('All form fields must be filled in')
         return;
     }
     let res = {}
-    res[event.target.elements[0].name]=event.target.elements[0].value.trim();
-    res[event.target.elements[1].name]=event.target.elements[1].value.trim();
+    res[event.target.elements.email.name]=event.target.email.value.trim();
+    res[event.target.elements.password.name]=event.target.password.value.trim();
     console.log(res)
     ourForm.reset()
 }
